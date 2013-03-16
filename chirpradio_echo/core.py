09a149331d9726d8b9e5d5ac44cfbae5d4e01306
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-03-15 20:30:06 -0500

    CHIRP Radio listener and echonest anaalyzer

--- chirpradio_echo/core.py
+++ chirpradio_echo/core.py
@@ -1,22 +1,114 @@
 from multiprocessing import (current_process, Manager, Lock, Pipe, Pool,
                              Process, Queue)
 import optparse
+import os
+import tempfile
 import time
 
-from .taskqlite import log, central_q, still_working, task
+from pyechonest import config as echonest_conf
+from pyechonest import song
+import requests
+
+from .taskqlite import log, central_q, task
+
+# Set a chunk size of roughly 908K.
+# This is about 40 seconds for 128kbps, give or take.
+# The mp3 stream's bitrate is not validated!
+file_size = 926343
+tmp_prefix = 'echoplex_'
+stream_url = 'http://chirpradio.org/stream'
+chirpradio_api = ('https://chirpradio.appspot.com/api/current_playlist'
+                  '?src=chirpradio-echo')
 
 
 @task
-def slicer(*args, **kw):
-    log.info('slicing...')
-    nested.delay()
-    time.sleep(5)
-    log.info('done')
+def listen(**kw):
+    run_gc()
+    res = requests.get(stream_url, stream=True, timeout=10)
+    log.info('Opening %s' % stream_url)
+    log.info('Writing temp files to %s/%s*.mp3' % (tempfile.gettempdir(),
+                                                   tmp_prefix))
+
+    dest = None
+    bufsize = 0
+    for chunk in res.iter_content(chunk_size=2048):
+        if not dest:
+            dest = tempfile.NamedTemporaryFile(delete=False,
+                                               prefix=tmp_prefix,
+                                               suffix='.mp3')
+        dest.write(chunk)
+        bufsize += len(chunk)
+        if bufsize >= file_size:
+            bufsize = 0
+            dest.close()
+            ask_chirpradio.delay(dest.name)
+            dest = None
 
 
 @task
-def nested(*args, **kw):
-    log.info('nested')
+def ask_chirpradio(filename, **kw):
+    try:
+        # This is a best effort guess at what's currently playing.
+        # The CHIRP stream is buffered and DJs are sometimes slow to
+        # update the track data. YMMV.
+        res = requests.get(chirpradio_api, headers={'Accept-Encoding': 'gzip'})
+        track_data = res.json()['now_playing']
+        log.info('%s: CHIRP says: %s: %s (%s)' % (os.path.split(filename)[1],
+                                                  track_data['artist'],
+                                                  track_data['track'],
+                                                  track_data['release']))
+        ask_echonest.delay(track_data, filename)
+    except:
+        os.unlink(filename)
+        raise
+
+
+@task
+def ask_echonest(chirpradio_id, filename, **kw):
+    try:
+        set_up_echonest()
+
+        fp = song.util.codegen(filename)
+        if len(fp) and "code" in fp[0]:
+            result = song.identify(query_obj=fp, version="4.11")
+            if len(result):
+                data = {'match': {'artist_name': result[0].artist_name,
+                                  'artist_id': result[0].artist_id,
+                                  'title': result[0].title,
+                                  'title_id': result[0].id}}
+            else:
+                data = {'match': None,
+                        'reason': 'This track may not be in the database yet.'}
+        else:
+            data = {'match': None,
+                    'reason': 'Could not read or decode file %s' % filename}
+        log.info('%s: echonest: %s' % (os.path.split(filename)[1], data))
+    finally:
+        os.unlink(filename)
+
+
+def run_gc():
+    """
+    Clean up any lingering temp files (probably from crashes)
+    """
+    expiry = 60 * 3
+    tmp = tempfile.gettempdir()
+    for fn in os.listdir(tmp):
+        if not fn.startswith(tmp_prefix):
+            continue
+        path = os.path.join(tmp, fn)
+        if os.stat(path).st_mtime < (time.time() - expiry):
+            os.unlink(path)
+
+
+_e_setup = False
+def set_up_echonest():
+    global _e_setup
+    if not _e_setup:
+        echonest_conf.ECHO_NEST_API_KEY = os.environ['ECHO_NEST_API_KEY']
+        p = '/usr/local/bin/echoprint-codegen'
+        echonest_conf.CODEGEN_BINARY_OVERRIDE = p
+        _e_setup = True
 
 
 def main():
@@ -26,8 +118,7 @@ def main():
                       'Default: %default')
     (opt, args) = p.parse_args()
     log.info('Starting workers')
-    for i in range(10):
-        slicer.delay()
+    listen.delay()
     central_q.work(concurrent_tasks=opt.qsize)
 
 
